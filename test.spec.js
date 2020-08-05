5c0841999bde663578025df0cc0145b88098b100
Bart Ledoux <bart.ledoux@rxvantage.com>
2020-08-05 09:34:24 -0500

    add a test

--- test.spec.js
+++ test.spec.js
@@ -2,4 +2,8 @@ describe("forward", () => {
   test("passing", () => {
     expect(typeof cellular).toBe("undefined");
   });
+
+  test("song2", () => {
+    expect(typeof cellular).toBe("undefined");
+  });
 });
